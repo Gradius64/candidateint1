9fcbabd637552cb75c3d84d381bf46ba878aeb78
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-11-08 11:23:57 -0600

    Add files via upload

--- Candidate.interface.tsx
+++ Candidate.interface.tsx
@@ -1,7 +1,7 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 
 
-interface Candidate {
+export interface Candidate {
     name: string;          // The name of the candidate
     username: string;      // The GitHub username of the candidate
     location: string;      // The location of the candidate
