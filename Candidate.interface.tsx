71166209f8b89261484f077e5d07c179ce545d89
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-11-08 11:17:39 -0600

    Add files via upload

---
+++ Candidate.interface.tsx
@@ -0,0 +1,29 @@
+// TODO: Create an interface for the Candidate objects returned by the API
+
+
+interface Candidate {
+    name: string;          // The name of the candidate
+    username: string;      // The GitHub username of the candidate
+    location: string;      // The location of the candidate
+    avatar: string;        // URL to the candidate's avatar image
+    email: string;         // The email address of the candidate
+    html_url: string;      // The URL to the candidate's GitHub profile
+    company: string;    
+ 
+
+
+   // The company the candidate is associated with
+    
+}
+
+
+    
+
+    
+    
+
+
+
+
+ 
+ 
